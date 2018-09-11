c5bf80bae6d4dc5241c47124a58839ff51deb3fb
Li-Hsin <lhchan@utu.fi>
2018-09-11 23:41:12 +0300

    formatting data.x (disambiguation & candidate generation)

--- src/sample.py
+++ src/sample.py
@@ -52,21 +52,39 @@ def _format_mentions_and_x0(can_list,men_list,men_padded):
     return mentions, x_zero
 
 def _format_x(can_list,x_zero,dict):
+    '''
+    can_list: list of list (mention level) of generated candidates
+    x_zero: list of numpy array of vectorized mentions
+    dict: vectorized controlled vocabulary
+    '''
     # x[1] = vectorized candidates
     x_one = []
+    debug_count_noncan = 0
+    # x[2] = candidate generation scores
+    x_two = []
+    #can_list>mention format: list of lists of n (key,score) tuples
     for mention in can_list:
-        for can in mention:
-            x_one.append(dict[])
-            #check can_list>mention format, check dict format, check terminology mapping
+        for can, score in mention:
+            x_one.append(dict.get(can,_non_canonical(can)))
+            x_two.append([score])
+        
+            #check dict format, check terminology mapping
+            #check how candidates are generated, if there's chance of non-canonical ids
+    logger.debug('{0} non-canonical forms used.'.format(debug_count_noncan))
 
             #pad the thing
 
-    # x[2] = candidate generation scores
-    x_two = []
+
 
     x = np.array([x_zero,x_one,x_two])
     return x
 
+def _non_canonical(id):
+    assert id not in dict
+    
+        debug_count_noncan += 1
+        return
 
+    disambiguated = 
 
 
